675ddb5c61a1640989fa3afaa0937d8f9ef5f0d7
stblt <staniguchi@bltinc.co.jp>
2026-05-11 19:09:22 +0900

    delete: .envファイルを削除

--- members/db.php
+++ members/db.php
@@ -1,16 +1,9 @@
 <?php
-$env = parse_ini_file(__DIR__ . '/../.env');
-
-define('DB_HOST', $env['DB_HOST']);
-define('DB_NAME', $env['DB_NAME']);
-define('DB_USER', $env['DB_USER']);
-define('DB_PASS', $env['DB_PASS']);
-
 try {
     $pdo = new PDO(
-        'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
-        DB_USER,
-        DB_PASS,
+        'mysql:host=localhost;dbname=nfqyowlb_2026;charset=utf8mb4',
+        'nfqyowlb_syain-ryokou',
+        'Bullbase5100-Bltinc',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
 } catch (PDOException $e) {
