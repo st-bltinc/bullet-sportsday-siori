9cdc94c93f0282dd10c69f586e2e7365fa6188b2
stblt <staniguchi@bltinc.co.jp>
2026-05-18 12:10:32 +0900

    fix: 言語をjaに変更

--- members/setup_add.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db.php';
-
-$pdo->exec("ALTER TABLE members ADD COLUMN birthday DATE NULL");
-$pdo->exec("ALTER TABLE members ADD COLUMN department VARCHAR(100) NULL");
-
-echo "カラム追加完了！";
-?>
